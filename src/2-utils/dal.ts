faadda0d660e393c697b4900b955fd7d4f1cf5e0
Yinon Bar <yinonbar1988@gmail.com>
2023-01-13 18:41:47 +0200

    add attendee route

--- src/2-utils/dal.ts
+++ src/2-utils/dal.ts
@@ -17,11 +17,21 @@ const dummyAttendees = JSON.stringify([
   },
 ]);
 
-export async function getAllAttendees(): Promise<AttendeeModel[]> {
+// get all attendee
+async function getAllAttendees(): Promise<AttendeeModel[]> {
   const attendees = JSON.parse(dummyAttendees);
   return attendees;
 }
 
+// add attendee
+async function addNewAttendee(attendee: AttendeeModel): Promise<AttendeeModel> {
+  const newattendee = JSON.parse(dummyAttendees[0]);
+  return newattendee;
+}
+
 // body: first_name: string, last_name, national_id, arrived: boolean}
 
-export default { getAllAttendees };
+export default {
+  getAllAttendees,
+  addNewAttendee,
+};
--- src/5-logic/attendeesLogic.ts
+++ src/5-logic/attendeesLogic.ts
@@ -6,3 +6,10 @@ export async function getAllAttendees(): Promise<AttendeeModel[]> {
   const attendees = await dal.getAllAttendees();
   return attendees;
 }
+// POST one attendee:
+export async function addSingleAttendee(
+  attendee: AttendeeModel
+): Promise<AttendeeModel> {
+  const newAttendee = await dal.addNewAttendee(attendee);
+  return newAttendee;
+}

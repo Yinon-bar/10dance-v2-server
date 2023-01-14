0288c2661b977203c4fd1f19727610e36910d79f
Yinon Bar <yinonbar1988@gmail.com>
2023-01-14 13:02:08 +0200

    add attendee

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import catchAll from "./3-middleware/catchAll";
+import routeNotFound from "./3-middleware/routeNotFound";
 import router from "./6-controller/router";
 
 const app = express();
@@ -6,6 +8,10 @@ const app = express();
 app.use(express.json());
 
 app.use("/api", router);
+
+app.use("*", routeNotFound);
+
+app.use(catchAll);
 
 app.listen(3001, () => {
   console.log("Listening to http://localhost:3001");

eafbe6b06bb22bd627cc534f8d55632011dbec19
Yaron Veg <Yaronveg1@gmail.com>
2023-01-14 13:14:59 +0200

    added stupiid log

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@ mongoose.set('strictQuery', false);
 mongoose.connect(
   `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
   async (err) => {
+    console.log("yes, yes it is!")
     if(err){console.log(err)}else { app.listen(process.env.PORT); console.log("Connected to DB and listening to port ",process.env.PORT)}  
   }
 );
